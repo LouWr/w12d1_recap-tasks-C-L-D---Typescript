bd28fcd97f64f7ec09b0c6b4e19f325012d60a15
C-L-D <clairedoswell1995@gmail.com>
2021-10-11 09:23:17 +0100

    Taks 1 recursion complete

--- task-1/index.js
+++ task-1/index.js
@@ -1,7 +1,21 @@
 //Use recursion to calculate the exponential value here (e.g. calculateExponent(4,1) === 4, calculateExponent(4,2) === 16, calculateExponent(4,3) === 64):
 
 function calculateExponent(baseNum, exponent) {
-  //👉 Write your code here!
+  // take in 2 numbers
+  // exponent is a counter
+  // stop recursion with ifs
+  // exp = 1, return 1
+  // exp = 0, return 0
+  // return baseNum * function(baseNum, exp-1)
+
+  if (exponent === 0) {
+    return 1;
+  }
+  if (baseNum === 0) {
+    return 0;
+  }
+
+  return baseNum * calculateExponent(baseNum, exponent - 1);
 }
 
 module.exports = { calculateExponent };
